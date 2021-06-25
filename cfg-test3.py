36f459c585c21ede525fba4a1379733fc1a35c54
John YE <johnye@metareco.com>
2021-06-25 21:12:16 +0800

    a cfg file
    Signed-off-by: John YE <johnye@metareco.com>

---
+++ cfg-test3.py
@@ -0,0 +1,50 @@
+from fifth import configurebase
+from fifth.common import update_if_not_none
+
+class Cfg(configurebase.ConfigureBase):
+    def __init__(self):
+        super(Cfg, self).__init__()
+
+
+    def init(self, video_input, threshold):
+        self.video_input = update_if_not_none(video_input, self.video_input)
+        self.threshold = update_if_not_none(threshold, self.threshold)
+
+        self.playlist = [
+
+            # {'keyframes'   :'motions/m0',
+            #  'reference'   :None,
+            #  'reference2'  :None,
+            #  'video_input' :video_input,
+            #  'threshold'   :2,
+            # },
+            
+            
+            {'keyframes'   :'motions/m3',
+             'reference'   :'motions/m3/reference.MP4',
+             'reference2'  :'motions/m3/reference_again.MP4',
+             'video_input' :self.video_input,
+             'threshold'   : 12,
+             'timeout': 350,
+             'feedback_interval'   :2,
+            },
+
+            {'keyframes'   :'motions/m4',
+             'reference'   :'motions/m4/reference.MP4',
+             'reference2'  :'motions/m4/reference_again.MP4',
+             'video_input' :self.video_input,
+             'threshold'   :2,
+             'timeout': 400,
+             'feedback_interval'   :3,                          
+            },
+
+            {'keyframes'   :'motions/m5',
+             'reference'   :'motions/m5/reference.MP4',
+             'reference2'  :'motions/m5/reference.MP4',
+             'video_input' :self.video_input,
+             'threshold'   :2,
+             'timeout': 400,
+             'feedback_interval'   :3,                                       
+            },
+        ]
+        
